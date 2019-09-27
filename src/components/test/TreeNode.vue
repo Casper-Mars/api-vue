bf48b8ed8585a65d85d30f67c3aa3234253ed8b1
capser <solitary@163.com>
2019-09-27 23:04:25 +0800

    添加树型节点

---
+++ src/components/test/TreeNode.vue
@@ -0,0 +1,28 @@
+<template>
+    <span>
+        <span>
+            <span>{{label}}</span>
+        </span>
+        <span>
+
+        </span>
+    </span>
+
+</template>
+
+<script>
+    export default {
+        name: "TestNode",
+        props:{
+          label:""
+        },
+        data(){
+            return{
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
