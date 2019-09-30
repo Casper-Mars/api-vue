afc5652ad7fa14cd36874cec2c4036d923ff6a17
casper <solitaryreach@163.com>
2019-09-30 10:21:46 +0800

    调试自定义树节点的样式

--- src/components/test/TreeNode.vue
+++ src/components/test/TreeNode.vue
@@ -1,15 +1,34 @@
 <template>
-
+    <!--    <div style="height: 50px">-->
+    <!--        <el-row type="flex" justify="space-between">-->
+    <!--            <el-col :span="12">-->
+    <!--                <el-row type="flex">-->
+    <!--                    <el-col :span="6">-->
+    <!--                        <span>{{label}}</span>-->
+    <!--                    </el-col>-->
+    <!--                </el-row>-->
+    <!--            </el-col>-->
+    <!--            <el-col :span="12">-->
+    <!--                <el-row type="flex" justify="end">-->
+    <!--                    <el-col v-if="refEnable" :span="14">-->
+    <!--                        <span>aaaaaa</span>-->
+    <!--                    </el-col>-->
+    <!--                    <el-col v-if="refEnable" :span="4">-->
+    <!--                        <span>-></span>-->
+    <!--                    </el-col>-->
+    <!--                </el-row>-->
+    <!--            </el-col>-->
+    <!--        </el-row>-->
+    <!--    </div>-->
     <span style="flex: 1; display: flex; align-items: center; justify-content: space-between; font-size: 14px; padding-right: 8px;">
-        <span>
-            <span>{{label}}</span>
-        </span>
-        <span v-if="refEnable" >
-            <el-input disabled>aaaa</el-input>
-            <el-button  type="primary">-></el-button>
-        </span>
-    </span>
-
+            <span>
+              <span>{{label}}</span>
+            </span>
+            <span v-if="refEnable">
+              <el-button style="font-size: 12px;" type="text">#1->user_sessionId</el-button>
+              <el-button style="font-size: 12px;" type="text">----></el-button>
+            </span>
+          </span>
 </template>
 
 <script>
@@ -17,7 +36,7 @@
         name: "TestNode",
         props: {
             label: String,
-            refEnable:Boolean
+            refEnable: Boolean
         },
         data() {
             return {
